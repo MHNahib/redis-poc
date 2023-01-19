b65cd564c454219d7473196476de0e2a20ab09f4
M. H. Nahib <mhnahib@gmail.com>
2023-01-19 22:17:30 +0600

    added error handeling on redis 🐜

--- src/service/redis.js
+++ src/service/redis.js
@@ -1,20 +1,22 @@
 const { createClient } = require("redis");
 const { redisPort, redisHost } = require("../config");
 
-const client = createClient({
-  host: redisHost,
-  port: redisPort,
-});
+let client;
 
 (async () => {
+  client = createClient({
+    host: redisHost,
+    port: redisPort,
+  });
+  client.on("connect", () => console.log("Connected with Redis!"));
+  client.on("error", (err) => console.log("Redis Client Error:  ", err));
+
   await client.connect();
-  client.on("connect", () => console.log("Connected!"));
-  client.on("error", (err) => console.log("Redis Client Error", err));
 })();
 
-const setDataInRedis = (key, value) => {
+const setDataInRedis = async (key, value) => {
   //   console.log(`${key}: ${value}`);
-  client.set(key, JSON.stringify(value));
+  await client.set(key, JSON.stringify(value));
 };
 
 const getDataFromRedis = async (key) => JSON.parse(await client.get(key));
